55f6dfda6394571dfb537a5d813b846d5a5ba79c
hala201 <halamurad2000@hotmail.com>
2025-02-13 20:10:57 -0500

    added autocommit feature

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export PATH=$(echo $PATH | sed -e 's/:\/mnt\/c\/Program Files\/nodejs//g')
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import { clear } from 'console';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+let activityLog : {time: Date, activity: string}[] = [];
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tracktonic" is now active!');
+	vscode.window.showInformationMessage('TrackTonic is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tracktonic.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TrackTonic!');
+	const change = vscode.commands.registerCommand('tracktonic.entry', () => {
+		vscode.workspace.onDidChangeTextDocument((e) => {
+			const entry = {time: new Date(), activity: e.document.uri.toString()};
+			activityLog.push(entry);
+			console.log('Activity log:', activityLog);
+		});
 	});
+	
+	context.subscriptions.push(change);
+	scheduleAutoCommit(context);
+}
 
-	context.subscriptions.push(disposable);
+function scheduleAutoCommit(context: vscode.ExtensionContext) {
+	const config = vscode.workspace.getConfiguration('tracktonic');
+	const intervalConfig = config.get<number>('autoCommitInterval') || 5;
+	const interval = intervalConfig * 1000 * 60;
+	const commitSchedule = setInterval(() => {
+		autoCommit();}, interval);
+	context.subscriptions.push({dispose: () => clearInterval(commitSchedule)});
+}
+
+function autoCommit() {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders || workspaceFolders.length === 0) {
+		console.warn('No workspace folder found');
+		return;
+	}
+	const workspaceFolder = workspaceFolders[0];
+	const commitmessage = `Auto commit:  ${+ new Date().toLocaleString()}`;
+	const gitCommand = `git add - A && commit -m "${commitmessage}"`;
+
+	cp.exec(gitCommand, {cwd: workspaceFolder.uri.fsPath}, (err, stdout, stderr) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(stdout);
+	});
 }
 
 // This method is called when your extension is deactivated
