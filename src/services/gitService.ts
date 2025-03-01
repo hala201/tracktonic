cf5a75918048518fce016934df8c4cba3e0c0d6b
hala201 <halamurad2000@hotmail.com>
2025-03-01 13:17:25 -0500

    added functionality for user to select workspace folder to track changes and created tracktonic repository.

--- src/services/gitService.ts
+++ src/services/gitService.ts
@@ -1,47 +1,90 @@
 import * as vscode from "vscode";
 import simpleGit, { SimpleGit } from "simple-git";
 import * as path from "path";
+import * as fs from "fs";
+import { get } from "http";
+
+async function getTrackTonicRepoPath(): Promise<string | undefined> {
+    const folders = vscode.workspace.workspaceFolders;
+    if (!folders || folders.length === 0) {
+        vscode.window.showErrorMessage("No open directories");
+        return undefined;
+    } else if (folders.length === 1) {
+        return path.join(folders[0].uri.fsPath, "tracktonic");
+    } else {
+        const pick : string | undefined = await vscode.window.showQuickPick(
+            folders.map((folder) => folder.uri.fsPath),
+            {placeHolder: "Select a folder tot store the TrackTonic repository"}
+        );
+        
+        if (pick) {
+            return path.join(pick, "tracktonic");
+        }
+        return undefined;
+    }
+    
+}
+
 
 export class GitService {
-    private gits : SimpleGit[];
+    private repoPath: string | undefined;
+    private git : SimpleGit;
     private commitInterval: NodeJS.Timeout | undefined;
     private changesMade: boolean = false;
 
     constructor() {
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-        if (!workspaceFolders || workspaceFolders.length === 0) {
-            throw new Error("No workspace folder open.");
+        this.initialize();
+    }
+    
+    private async initialize() {
+        this.repoPath = await getTrackTonicRepoPath();
+        if (!this.repoPath) {
+            throw new Error("Track tonic repository path is undefined");
         }
-        this.gits = workspaceFolders.map(folder => simpleGit(folder.uri.fsPath));
+        
+        await this.ensureTrackTonicRepo();
+        this.git = simpleGit(this.repoPath);
+    }
+
+    private async ensureTrackTonicRepo() {
+        if (!this.repoPath) {
+            return;
+        }
+        if (!fs.existsSync(this.repoPath)) {
+            fs.mkdirSync(this.repoPath);
+            vscode.window.showInformationMessage(`Created tracktonic repository at ${this.repoPath}`);
+        }
+
+        if (!fs.existsSync(path.join(this.repoPath, ".git"))) {
+            const git = simpleGit(this.repoPath);
+            await git.init();
+            vscode.window.showInformationMessage("Initialized tracktonic git repository");
+        }
     }
 
     async checkGitStatus(): Promise<boolean> {
         this.changesMade = false;
-        for (const git of this.gits) {
-            try {
-                const status = await git.status();
-                this.changesMade = status.files.length > 0;
-            } catch (error) {
-                vscode.window.showErrorMessage("Error checking git status.");
-            }
+        try {
+            const status = await this.git.status();
+            this.changesMade = status.files.length > 0;
+        } catch (error) {
+            vscode.window.showErrorMessage("Error checking git status.");
         }
         return this.changesMade;
     }
 
     async handleCommit() : Promise<void> {
         let reposCommitted = 0;
-        for (const git of this.gits) {
-            try {
-                const status = await git.status();
-                if (status.files.length > 0) {
-                    await git.add(".");
-                    await git.commit("Working on git commit message for later use");
-                    await git.push();
-                    reposCommitted++;
-                }
-            } catch (error) {
-                vscode.window.showErrorMessage(`Git commit failed`);
+        try {
+            const status = await this.git.status();
+            if (status.files.length > 0) {
+                await this.git.add(".");
+                await this.git.commit("Working on git commit message for later use");
+                await this.git.push();
+                reposCommitted++;
             }
+        } catch (error) {
+            vscode.window.showErrorMessage(`Git commit failed`);
         }
         
         if (reposCommitted > 0) {
