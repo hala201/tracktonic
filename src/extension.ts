35f8c221e8146fc4acb344b740eb62fb5b9d014a
hala201 <halamurad2000@hotmail.com>
2025-03-19 14:26:20 -0400

    feat(extension): Remove shell logging setup
    Removes the `setupShellLogging` function and related code from the extension.  This function was responsible for modifying shell configuration files (.bashrc, .zshrc, .config/fish/config.fish) to enable terminal logging.

--- src/extension.ts
+++ src/extension.ts
@@ -2,36 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { GitService } from './services/gitService';
-import * as fs from "fs";
-import * as path from "path";
-import { exec } from "child_process";
-
-export async function setupShellLogging() {
-    const homeDir = process.env.HOME || process.env.USERPROFILE;
-    const bashrcPath = path.join(homeDir!, ".bashrc");
-    const zshrcPath = path.join(homeDir!, ".zshrc");
-    const fishPath = path.join(homeDir!, ".config/fish/config.fish");
-
-    const logCommand = `export PROMPT_COMMAND='echo "$(date +"%Y-%m-%d %H:%M:%S") $USER: $BASH_COMMAND" >> ~/.vscode_terminal_log'`;
-
-    if (fs.existsSync(bashrcPath)) {
-        fs.appendFileSync(bashrcPath, `\n${logCommand}\n`);
-    }
-    if (fs.existsSync(zshrcPath)) {
-        fs.appendFileSync(zshrcPath, `\npreexec() { echo "$(date +"%Y-%m-%d %H:%M:%S") $USER: $1" >> ~/.vscode_terminal_log; }\n`);
-    }
-    if (fs.existsSync(fishPath)) {
-        fs.appendFileSync(fishPath, `\nfunction log_command --on-event fish_preexec\n echo (date "+%Y-%m-%d %H:%M:%S") (whoami) (pwd) $argv >> ~/.vscode_terminal_log\nend\n`);
-    }
-
-    vscode.window.showInformationMessage("TrackTonic: Terminal logging enabled. Restart your terminal for changes to take effect.");
-}
-
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage("TrackTonic is activated!");
 
-    setupShellLogging();
     const gitService = new GitService(context);
     const intervalMs = gitService.getCommitInterval();
     gitService.startAutoCommit(intervalMs);
